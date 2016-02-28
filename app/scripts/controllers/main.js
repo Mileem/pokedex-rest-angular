1693520c4c919ba7f0d6a7076a4f0ac60bac43d9
Sarah Ghidalia <Mileem@macbook-air-de-sarah.home>
2016-02-28 09:46:55 +0100

    Initial commit yeaoman angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokedexRestAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pokedexRestAngularApp
+ */
+angular.module('pokedexRestAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
